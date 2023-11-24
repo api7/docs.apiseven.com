b811ca68731c0da5c46eecdb2fb29126040aa553
Qi Guo <979918879@qq.com>
2023-11-24 12:31:04 +0800

    fix: hide 3.2.2 version docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,6 +96,8 @@ const config = {
             projectName: 'enterprise_versioned_docs',
           }),
         includeCurrentVersion: false,
+        // 放开多版本后移除该配置
+        onlyIncludeVersions: ['2.13.2304'],
       }
     ],
     [
