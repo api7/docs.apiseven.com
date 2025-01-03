62a165012cd810b8b0926371dd50712e09d9b24b
Qi Guo <979918879@qq.com>
2025-01-03 14:30:15 +0800

    feat: archive old version docs

---
+++ config/enterprise-version.ts
@@ -0,0 +1,17 @@
+const archivedVersions = [
+  {
+    type: 'html',
+    value: '<hr class="dropdown-separator">',
+  },
+  {
+    type: 'html',
+    className: 'dropdown-archived-versions',
+    value: 'Archived versions',
+  },
+  {
+    label: 'test',
+    href: 'https://docs-archive.api7.ai/enterprise/introduction',
+  },
+];
+
+export default archivedVersions;
